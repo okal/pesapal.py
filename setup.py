1cd56b9787b1966ea9fbef577f4851ec7e40ef25
Okal Otieno <okal@justokal.com>
2012-06-04 11:10:03 +0300

    Add setup file for ez_install or pip.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pesapal',
+    version='0.1',
+    description='A library for the Pesapal API.',
+    author='Okal Otieno',
+    author_email='okal+python@justokal.com',
+    url='https://github.com/okal/pesapal.py',
+    packages=find_packages(),
+    license='MIT License',
+    zip_safe=True)
